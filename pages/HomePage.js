1f791dee3a648c30af7da953569921e9f469497c
Raj Tera <swarajtera52@gmail.com>
2024-01-26 15:19:31 +0600

    Stored login data in async storage and fetched them in home page

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,23 +1,47 @@
-import React, { useEffect } from 'react';
-import { View, ImageBackground, StyleSheet, Text } from 'react-native';
-import { Appbar } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
-
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, ImageBackground, StyleSheet, Text } from "react-native";
+import { Appbar } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  //codes for nvigation to another page after timeout
+  // useEffect(() => {
+  //   console.log("HomePage");
+  //   const timer = setTimeout(() => {
+  //     navigation.navigate("Login"); // Redirect to the Login screen after 3 seconds
+  //   }, 1000);
 
-  useEffect(() => {
-    console.log("HomePage");
-    const timer = setTimeout(() => {
-      navigation.navigate('Login'); // Redirect to the Login screen after 3 seconds
-    }, 1000);
+  //   return () => clearTimeout(timer); // Clear the timer if the component unmounts
+  // }, [navigation]);
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const storedUsername = await AsyncStorage.getItem("username");
+          const storedEmail = await AsyncStorage.getItem("email");
 
-    return () => clearTimeout(timer); // Clear the timer if the component unmounts
-  }, [navigation]);
+          if (storedUsername && storedEmail) {
+            setUsername(storedUsername);
+            setEmail(storedEmail);
+          }
+        } catch (error) {
+          console.error("Error fetching data from AsyncStorage:", error);
+        }
+      };
+
+      fetchData();
+    }, []) // The empty dependency array ensures this effect runs when the Home screen comes into focus
+  );
 
   return (
     <ImageBackground
-      source={{ uri: 'https://hips.hearstapps.com/hmg-prod/images/online-buying-and-delivery-concept-royalty-free-image-1675370119.jpg?crop=0.563xw:1.00xh;0.216xw,0&resize=640:*' }}
+      source={{
+        uri: "https://hips.hearstapps.com/hmg-prod/images/online-buying-and-delivery-concept-royalty-free-image-1675370119.jpg?crop=0.563xw:1.00xh;0.216xw,0&resize=640:*",
+      }}
       style={styles.backgroundImage}
     >
       <View style={styles.overlay}>
@@ -25,7 +49,10 @@ const HomeScreen = () => {
           <Appbar.Content title="Home" />
         </Appbar.Header> */}
         <View style={styles.content}>
-          <Text>Your Home Page Content</Text>
+          <View>
+            <Text>Welcome, {username}!</Text>
+            <Text>Email: {email}</Text>
+          </View>
         </View>
       </View>
     </ImageBackground>
@@ -35,8 +62,8 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
   },
   overlay: {
     flex: 1,
@@ -44,8 +71,8 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
--- pages/RegisterScreen.js
+++ pages/RegisterScreen.js
@@ -1,76 +1,86 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Appbar, TextInput as PaperTextInput, Button as PaperButton, IconButton, Checkbox as PaperCheckbox, Snackbar} from 'react-native-paper';
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import {
+  Appbar,
+  TextInput as PaperTextInput,
+  Button as PaperButton,
+  IconButton,
+  Checkbox as PaperCheckbox,
+  Snackbar,
+} from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const RegisterScreen = ({ navigation }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [usernameError, setUsernameError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
-const [agreeToTerms, setAgreeToTerms] = useState(false);
-const [agreeToTermsError, setAgreeToTermsError] = useState('');
-const [isToastVisible, setToastVisible] = useState(false);
-const toggleToast = () => {
-  setToastVisible(!isToastVisible);
-   // navigation.navigate('Login');
-};
+  const [agreeToTerms, setAgreeToTerms] = useState(false);
+  const [agreeToTermsError, setAgreeToTermsError] = useState("");
+  const [isToastVisible, setToastVisible] = useState(false);
+  const toggleToast = () => {
+    setToastVisible(!isToastVisible);
+    // navigation.navigate('Login');
+  };
   const validateUsername = () => {
-  
- if (!username) {
-    setUsernameError('Please enter your username');
-  }  else {
-    setUsernameError('');
-  }
+    if (!username) {
+      setUsernameError("Please enter your username");
+    } else {
+      setUsernameError("");
+    }
   };
 
   const validatePassword = () => {
-    const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,10}$/;
+    const passwordRegex =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,10}$/;
 
     if (!password) {
-      setPasswordError('Please enter your password');
-  }
-    else if (!passwordRegex.test(password)) {
-      setPasswordError('Password must be 6-10 characters with at least one number and one special character');
-    }else {
-      setPasswordError('');
+      setPasswordError("Please enter your password");
+    } else if (!passwordRegex.test(password)) {
+      setPasswordError(
+        "Password must be 6-10 characters with at least one number and one special character"
+      );
+    } else {
+      setPasswordError("");
     }
   };
-const validateEmail = () => {
-          const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
+  const validateEmail = () => {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
 
- if (!email) {
-    setEmailError('Please enter your Email');
-  }
-  else if (!emailRegex.test(email)) {
-    setEmailError('Invalid email address');
-  }
-  else {
-    setEmailError('');
-  }
-
-};
+    if (!email) {
+      setEmailError("Please enter your Email");
+    } else if (!emailRegex.test(email)) {
+      setEmailError("Invalid email address");
+    } else {
+      setEmailError("");
+    }
+  };
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-  const handleRegister = () => {
+  const handleRegister = async () => {
     validateUsername();
     validatePassword();
     validateEmail();
-  if (!agreeToTerms) {
-    setAgreeToTermsError('Please agree to the terms and services');
-    return;
-  } else {
-    setAgreeToTermsError('');
-  }
+    if (!agreeToTerms) {
+      setAgreeToTermsError("Please agree to the terms and services");
+      return;
+    } else {
+      setAgreeToTermsError("");
+    }
 
     if (!usernameError && !passwordError && !agreeToTermsError && !emailError) {
-      // Implement your registration logic here
-      //console.log('Register succesful! Tap to continue');
+      try {
+        await AsyncStorage.setItem("username", username);
+        await AsyncStorage.setItem("password", password);
+        await AsyncStorage.setItem("email", email);
         toggleToast();
+      } catch (error) {
+        console.error("Error saving data in AsyncStorage:", error);
+      }
     }
   };
 
@@ -87,7 +97,9 @@ const validateEmail = () => {
           onChangeText={(text) => setUsername(text)}
           onBlur={validateUsername}
         />
-        {usernameError ? <Text style={styles.errorText}>{usernameError}</Text> : null}
+        {usernameError ? (
+          <Text style={styles.errorText}>{usernameError}</Text>
+        ) : null}
 
         <PaperTextInput
           label="Password"
@@ -98,33 +110,33 @@ const validateEmail = () => {
           onBlur={validatePassword}
           right={
             <PaperTextInput.Icon
-              name={passwordVisible ? 'eye-off' : 'eye'}
-              icon="eye"   
+              name={passwordVisible ? "eye-off" : "eye"}
+              icon="eye"
               onPress={togglePasswordVisibility}
-               style={styles.iconStyle} 
+              style={styles.iconStyle}
             />
           }
         />
-        {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
+        {passwordError ? (
+          <Text style={styles.errorText}>{passwordError}</Text>
+        ) : null}
 
         <PaperTextInput
           label="Email"
           style={styles.input}
           value={email}
           onChangeText={(text) => setEmail(text)}
-           onBlur={validateEmail}
+          onBlur={validateEmail}
         />
-        {emailError ? (
-  <Text style={styles.errorText}>{emailError}</Text>
-) : null}
-<PaperCheckbox.Item
-  label="I agree to the terms and services"
-  status={agreeToTerms ? 'checked' : 'unchecked'}
-  onPress={() => setAgreeToTerms(!agreeToTerms)}
-/>
-{agreeToTermsError ? (
-  <Text style={styles.errorText}>{agreeToTermsError}</Text>
-) : null}
+        {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
+        <PaperCheckbox.Item
+          label="I agree to the terms and services"
+          status={agreeToTerms ? "checked" : "unchecked"}
+          onPress={() => setAgreeToTerms(!agreeToTerms)}
+        />
+        {agreeToTermsError ? (
+          <Text style={styles.errorText}>{agreeToTermsError}</Text>
+        ) : null}
         <PaperButton
           mode="contained"
           style={styles.button}
@@ -133,19 +145,18 @@ const validateEmail = () => {
           Register
         </PaperButton>
         <Snackbar
-  visible={isToastVisible}
-  onDismiss={toggleToast}
-  duration={3000} // Adjust the duration as needed
-  action={{
-    label: 'OK',
-       onPress: () => {
-      navigation.navigate('Login'); // Navigate to the login page when the toast is pressed
-    },
-
-  }}
->
-  Registration successful!
-</Snackbar>
+          visible={isToastVisible}
+          onDismiss={toggleToast}
+          duration={3000} // Adjust the duration as needed
+          action={{
+            label: "OK",
+            onPress: () => {
+              navigation.navigate("Home"); // Navigate to the login page when the toast is pressed
+            },
+          }}
+        >
+          Registration successful!
+        </Snackbar>
       </View>
     </View>
   );
@@ -157,17 +168,17 @@ const styles = StyleSheet.create({
     margin: 12,
   },
   button: {
-    width: '50%',
-    alignSelf: 'center',
+    width: "50%",
+    alignSelf: "center",
     marginTop: 20,
   },
   errorText: {
-    color: 'red',
+    color: "red",
     marginLeft: 16,
     marginTop: 4,
   },
   iconStyle: {
-    color: 'blue',
+    color: "blue",
     fontSize: 24,
   },
 });
